22c7f2068a779d5d045d3cec70f61e4c8652de68
Petko Petkov <p.petkov@zaralab.org>
2017-03-19 19:02:02 +0200

    implemented RCT

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
 # bgsensor
+
+-- Run the script
+create a file named ttn.json and insert the values:
+
+{
+    "TTN_REGION": "eu",
+    "TTN_APPID": "app-id",
+    "TTN_ACCESSKEY": "app-key"
+};
+
+
+then:
+
+npm install
+npm start
+
+
+time format:
+[20,17,2,19,16,56,29]
+
+your encoder function
+
+function Encoder(object, port) {
+  var bytes = [];
+  if (port === 1) {
+    if(object.time){
+      var now = new Date();
+      var year = now.getUTCFullYear() + '';
+      var firstYByte = ~~(year.substr(0, year.length -2));
+      var lastYByte = ~~(year.substr(year.length -2));
+
+      bytes[0] = firstYByte;
+      bytes[1] = lastYByte;
+      bytes[2] = now.getUTCMonth();
+      bytes[3] = now.getUTCDate();
+      bytes[4] = now.getUTCHours();
+      bytes[5] = now.getUTCMinutes();
+      bytes[6] = now.getUTCSeconds();
+    }
+  }
+
+  return bytes;
+}
+
+your decoder function
+
+function Decoder(bytes, port) {
+  var decoded = {};
+
+  if (port === 1){
+    if(bytes[0] = 0) {
+      decoded.time = true;
+    }
+  }
+
+  return decoded;
+}
---
+++ server.js
@@ -0,0 +1,51 @@
+var ttn = require('ttn');
+
+var region,
+    appId,
+    accessKey;
+
+var initTTN = function(config) {
+    var client = new ttn.Client(config.TTN_REGION, config.TTN_APPID, config.TTN_ACCESSKEY);
+
+    client.on('connect', function(connack) {
+        console.info('[DEBUG]', 'Connect:', connack);
+    });
+
+    client.on('error', function(err) {
+        console.error('[ERROR]', err.message);
+    });
+
+    client.on('activation', function(deviceId, data) {
+        console.info('[INFO] ', 'Activation:', deviceId, data);
+    });
+
+    /*
+     *
+     * null = all devices *, 'led' = field to listen for
+     */
+    client.on('message', null, 'time', function(deviceId, flag) {
+
+        // Set the time in the format
+        var payload = {
+            time: true
+        };
+
+        // If you don't have an encoder payload function:
+        var payload = [];
+        var now = new Date();
+        var year = now.getUTCFullYear() + '';
+        var firstYByte = ~~(year.substr(0, year.length -2));
+        var lastYByte = ~~(year.substr(year.length -2));
+
+        payload[0] = firstYByte;
+        payload[1] = lastYByte;
+        payload[2] = now.getUTCMonth();
+        payload[3] = now.getUTCDate();
+        payload[4] = now.getUTCHours();
+        payload[5] = now.getUTCMinutes();
+        payload[6] = now.getUTCSeconds();
+
+        console.info('[DEBUG]', 'Sending:', JSON.stringify(payload));
+        client.send(deviceId, payload);
+    });
+};
